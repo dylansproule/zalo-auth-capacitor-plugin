6ad73626320cb661533d2b34e7f7436d192fb49c
Dylan Sproule <dylan.sproule@gmail.com>
2023-11-26 09:25:38 +0700

    Update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,14 @@
-import nodeResolve from '@rollup/plugin-node-resolve';
-
 export default {
   input: 'dist/esm/index.js',
   output: {
     file: 'dist/plugin.js',
     format: 'iife',
-    name: 'capacitorPlugin', // TODO: change this
+    name: 'zalo-capacitor-plugin',
     globals: {
       '@capacitor/core': 'capacitorExports',
     },
     sourcemap: true,
   },
   plugins: [
-    nodeResolve({
-      // allowlist of dependencies to bundle in
-      // @see https://github.com/rollup/plugins/tree/master/packages/node-resolve#resolveonly
-      resolveOnly: ['lodash'],
-    }),
   ],
 };
